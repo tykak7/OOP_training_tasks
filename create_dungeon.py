0b7063e49e9743f30b8053d38a5437e8af085d49
tykak7 <150191653+tykak7@users.noreply.github.com>
2023-12-15 21:21:38 +0600

    Translate to ENG

--- create_dungeon.py
+++ create_dungeon.py
@@ -1,33 +1,33 @@
-import random #generování náhodných čísel
+import random #random number generation
 
 
-# vytvoří 2D bludiště:
+# creates a 2D maze:
 def create_maze(width, height, max_tunnels):
     maze = [['▓' for _ in range(width)] for _ in range(height)]
 
-    # startovní pozice je (1, 1)
+    # the starting position is (1, 1)
     maze[1][1] = '.'
 
-    # sledovaní aktuální pozici v bludišti
+    # tracking your current position in the maze
     current_position = (1, 1)
-    # všichni možné směry: doprava, doleva, nahoru, dolů
+    # all possible directions: right, left, up, down
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-# vygenerujeme náhodnou délku tunelu
+# generate a random tunnel length
     for _ in range(max_tunnels):
         direction = random.choice(directions)
         length = random.randint(1, max(width, height))
 
-# uvnitř smyčky se pro každý krok v tunelu kontroluje nová pozice:
+# inside the loop, a new position is checked for each step in the tunnel:
         for _ in range(length):
             new_position = (current_position[0] + direction[0], current_position[1] + direction[1])
 
-# je-li nová pozice uvnitř labyrintu, na tuto pozici umístěn symbol tečky:
+# if there is a new position inside the labyrinth, the dot symbol is placed at that position:
             if 0 < new_position[0] < height - 1 and 0 < new_position[1] < width - 1:
                 maze[new_position[0]][new_position[1]] = '.'
                 current_position = new_position
 
-# vytvoření stěn kolem labyrintu
+# creating walls around the labyrinth
     for i in range(width):
         maze[0][i] = '▓'
         maze[height - 1][i] = '▓'
@@ -37,16 +37,16 @@ def create_maze(width, height, max_tunnels):
 
     return maze
 
-# vypsání bludiště do konzoly
+# listing the maze in the console
 def print_maze(maze):
     for row in maze:
         print(' '.join(row))
 
-# Nastavení velikosti bludiště a maximálního počtu tunelů
+# Set maze size and maximum number of tunnels
 width = 31
 height = 11
 max_tunnels = 43
 
-# Generování a zobrazení bludiště
+# Maze generation and display
 maze = create_maze(width, height, max_tunnels)
 print_maze(maze)
